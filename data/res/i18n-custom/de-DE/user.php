d3831a00666549d0e9537ec7547684e05a49233b
Busfreak <martin@middeke.de>
2020-06-05 15:52:46 +0200

    Send Activation Mail to User
    Send Activation Mail to User

---
+++ data/res/i18n-custom/de-DE/user.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+    'Your account has been activated' => 'Dein Benutzerkonto wurde aktiviert',
+    'Your user account has been activated. You can now login with your email address and password. Have fun!'
+        => 'Dein Benutzerkonto wurde erfolgreich aktiviert. Du kannst dich nun anmelden. Viel Spaß!',
+
+);
